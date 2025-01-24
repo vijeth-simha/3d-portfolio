09887804b60b9b1c0ab65dccfdb0d32d386a5f8d
vijeth_simha <vijethsimha71@gmail.com>
2025-01-24 21:10:40 +0530

    i am able to run the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import { useState } from "react";
-
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 const App = () => {
-  return <div>3D Developer Portfolio</div>;
+  return <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+    </div>
+  </BrowserRouter>;
 };
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  vite: {
-    server: {
-        fs: {
-            allow: ["F:/#playground/3d_portfolio"]
-        }
-    }
-}
 })
