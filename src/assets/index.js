533d9968f2a865fd370ab319575df2db982caf7e
vijeth_simha <vijethsimha71@gmail.com>
2025-01-25 11:32:13 +0530

    nav code added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import newLogo from "./new-logo-copy.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -32,6 +33,7 @@ import tripguide from "./tripguide.png";
 
 export {
   logo,
+  newLogo,
   backend,
   creator,
   mobile,
