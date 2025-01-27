b80496f8676ee6efc1108c2a58ad4e92bf4de4fd
vijeth_simha <vijethsimha71@gmail.com>
2025-01-27 20:45:08 +0530

    started with about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,39 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
-import { fadeIn, textVariant } from "../utils";
+import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = ({ title, index }) => {
+  return <p>{title}</p>;
+};
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[-30px]"
+      >
+        I&apos;m a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I&apos;m a quick learner and collaborate closely with clients
+        to create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let&apos;s work together to bring your ideas to
+        life!
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={index} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
