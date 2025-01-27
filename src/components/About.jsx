5271c33811237aae3d5851b96ae6bcad52d3c3e0
vijeth_simha <vijethsimha71@gmail.com>
2025-01-27 22:45:23 +0530

    started with HOC

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,34 @@
 /* eslint-disable react/prop-types */
-import React from "react";
 import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({ title, index }) => {
-  return <p>{title}</p>;
+const ServiceCard = ({ title, index, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-3xl shadow-card"
+      >
+        <div
+          // eslint-disable-next-line react/no-unknown-property
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[-20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
 };
 const About = () => {
   return (
