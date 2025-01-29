d965ab0d1679018d4a0f0e83df7179f1b3732fb8
vijeth_simha <vijethsimha71@gmail.com>
2025-01-29 19:53:25 +0530

    fadein animation from left bug is solved

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,8 +11,8 @@ const ServiceCard = ({ title, index, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-        className="w-full green-pink-gradient p-[1px] rounded-3xl shadow-card"
+        variants={fadeIn("right", "spring", 1.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
           // eslint-disable-next-line react/no-unknown-property
