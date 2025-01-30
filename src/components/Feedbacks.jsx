74c4fa49c6ce82f03346d6bb2557e7d348a5e18b
vijeth_simha <vijethsimha71@gmail.com>
2025-01-30 22:48:24 +0530

    testimonials is done

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -19,7 +19,7 @@ const FeedbackCard = ({
       variants={fadeIn("", "spring", index * 0.75, 0.75)}
       className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
     >
-      <p className="text-white font-black text-[48px]">"</p>
+      <p className="text-white font-black text-[48px]">&quot;</p>
       <div className="mt-1">
         <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
         <div className="mt-7 flex justify-between items-center gap-1">
@@ -58,7 +58,7 @@ const Feedbacks = () => {
           <FeedbackCard
             key={testimonial.name}
             index={index}
-            testimonial={testimonial}
+            {...testimonial}
           />
         ))}
       </div>
