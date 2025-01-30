ffc4f31841ac48b0744fa9f0a0d75a06e68af19b
vijeth_simha <vijethsimha71@gmail.com>
2025-01-30 20:51:12 +0530

    started testimonials

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { testimonials } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Feedbacks = () => {
   return (
