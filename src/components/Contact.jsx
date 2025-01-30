f55223ab76ff1b77b614d7c0c9e65a4e4db05f2b
vijeth_simha <vijethsimha71@gmail.com>
2025-01-30 22:54:58 +0530

    contact secion started

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+import emailjs from "@emailjs/browser";
+import { EarthCanvas } from "./canvas";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  return <div>Contact</div>;
+};
 
-export default Contact
+export default Contact;
