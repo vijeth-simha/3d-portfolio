a4be8825054e9e86bec3e9f65d6302b573b63de3
vijeth_simha <vijethsimha71@gmail.com>
2025-02-01 10:32:28 +0530

    started with earth model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,25 +1,23 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import {
-  OrbitControls,
-  Preload,
-  useGLTF,
-} from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../loader";
 
-
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+  return <div>Earth</div>;
+};
 
-const EarthCanvas = ()=>{
+const EarthCanvas = () => {
   return (
-    <Canvas>
-
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      
     </Canvas>
-  )
-}
+  );
+};
 
-export default EarthCanvas
+export default EarthCanvas;
