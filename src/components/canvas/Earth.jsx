409fdb95a2ad49ae8215f17075fd2d8d084ef1db
vijeth_simha <vijethsimha71@gmail.com>
2025-02-01 12:18:30 +0530

    earth model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/no-unknown-property */
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../loader";
 
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF("./planet/scene.gltf");
+  return <primitive object={earth.scene} scale={2.4} position-y={0} rotation-y={0} />;
 };
 
 const EarthCanvas = () => {
@@ -13,7 +15,7 @@ const EarthCanvas = () => {
       shadows
       frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{ fov: 45, near: 0.1, for: 200, position: [-4, 3, 6] }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
